f461ec2de4c39b6cd14aa6b9488a7da6ecd0105b
jin <hevi.viyan3@gmail.com>
2022-12-26 11:23:14 +0100

    promt

---
+++ app.js
@@ -0,0 +1,23 @@
+// Condition-Karar yapilari
+
+//? Console`dan girilen bir sayinin pozitif , negatif 
+// ?yada 0 oldugunu tespit ederek yazdirabiliriz
+
+
+// const sayi = promt ("bir sayi giriniz:")
+
+// console.log(sayi , typeof sayi);
+
+// if(sayi > 0 ){
+//     console.log(`${sayi} sayisi pozitif bir sayidir...` );
+// }else if(sayi < 0){
+//     console.log(`${sayi} sayisi negatif bit sayidir`);
+// }else{
+//     console.log(`${sayi} sifira esittir`);
+// }
+
+//? Ornek2: Console`den 2 sayi alarak bunlarin en büyügünü yazdiriniz
+
+const n1 = +promt ("1.sayiyi giriniz:");
+const n2 = +promt ("2.sayiyi giriniz:");
+const n3 = +promt ("3.sayiyi giriniz:");
