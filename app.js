712a531b4117e288e7272c98dc482ad9be29143a
jin <hevi.viyan3@gmail.com>
2022-12-26 11:28:18 +0100

    kücük büyük

--- app.js
+++ app.js
@@ -21,3 +21,11 @@
 const n1 = +promt ("1.sayiyi giriniz:");
 const n2 = +promt ("2.sayiyi giriniz:");
 const n3 = +promt ("3.sayiyi giriniz:");
+
+if (n1 >= n2 && n1 >= n3){
+    console.log(`${n1} is biggest`);
+}else if (n2 >= n1 && n2 >= n3){
+    console.log(`${n2} is biggest`);
+}else if (n3 >= n1 && n3 >= n2){
+    console.log(`${n3} is biggest`);
+}
